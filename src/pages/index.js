f0ef0c38c2fa479802d8163b32f9cb46372154fd
faizfak <faizfak@gmail.com>
2021-07-01 15:17:11 +0700

    asda 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,14 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <p>Now go build something great.</p>
     <p>Now go build something great.</p>
