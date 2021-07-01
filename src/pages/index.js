672429827fa8a0c40cbfd51471413a4c6eff695e
faizfak <faizfak@gmail.com>
2021-07-01 15:16:25 +0700

    Revert "asdaw"
    This reverts commit d2f066179d70dbbd508c7fda7b206e602b2919dd.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,6 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <p>Now go build something great.</p>
-    <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
